1b01c5ac7c6d067e7bff226f89adfefbf6590997
ithodol <ddmdjesz1@gmail.com>
2024-12-09 09:37:16 +0900

    시험 선택2

--- day19/4_시험선택2.js
+++ day19/4_시험선택2.js
@@ -6,3 +6,47 @@
     게시물 상세
     day13 중간과제7 + day14 중간과제8
 */
+
+// 회원가입
+function join(){
+
+}
+
+
+
+
+
+
+// 로그인
+function login(){
+
+}
+
+
+
+
+
+
+// 게시물 쓰기
+function write(){
+
+}
+
+
+
+
+
+// 게시물 목록
+function list(){
+    
+}
+
+
+
+
+
+
+
+
+
+// 게시물 상세
